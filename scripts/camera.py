5bcaf82187eb6da96b724bc88bed966456ecc123
blackhker <bhkdos@gmail.com>
2025-01-08 16:33:27 +0800

    删除划线外参标定

--- scripts/camera.py
+++ scripts/camera.py
@@ -1,7 +1,6 @@
 import os
 import numpy as np
 import cv2
-import math
 
 
 class Camera:
@@ -32,8 +31,6 @@ class Camera:
                 tmp1[4] = params[3, 2]
                 self.Distortion = tmp1
 
-                # print(tmp1)
-
                 if n >= 9:
                     tmp2 = np.zeros((4, 4))
                     tmp2[0:3, 0:3] = params[5:8, 0:3].copy()
@@ -43,11 +40,6 @@ class Camera:
                     self.rotM = params[5:8, 0:3].copy()
                     self.rvec = np.reshape(cv2.Rodrigues(self.rotM)[0], 3)
                     self.tvec = params[8].copy()
-                    # print(self.IntrinsicMatrix)
-                    # print(self.ExtrinsicsMatrix)
-                    # print(self.rotM)
-                    # print(self.rvec)
-                    # print(self.tvec)
 
                     tmp1 = np.zeros((3, 4))
                     tmp1[0:3, 0:3] = self.IntrinsicMatrix.copy()
@@ -65,6 +57,11 @@ class Camera:
         np.savetxt(filename, res, delimiter=',')
 
     def transform(self, world_coord):
+        """变换世界坐标到图像坐标
+
+        :param world_coord:
+        :return:
+        """
         if not self._flag:
             print('相机参数未标定')
             return
@@ -73,21 +70,6 @@ class Camera:
         y = img_coord[0][0][0][1]
         return round(x), round(y)
 
-        # world_coord = np.r_[world_coord, image]
-        # x, y, t, _ = np.matmul(self.ExtrinsicsMatrix, world_coord)
-        # x = x / t
-        # y = y / t
-        # r = math.sqrt(x * x + y * y)
-        # k1 = self.Distortion[0]
-        # k2 = self.Distortion[image]
-        # p1 = self.Distortion[2]
-        # p2 = self.Distortion[3]
-        # k3 = self.Distortion[4]
-        # x_ = x * (image + k1 * math.pow(r, 2) + k2 * math.pow(r, 4) + k3 * math.pow(r, 6)) + 2 * p1 * x * y + p2 * (r * r + 2 * x * y)
-        # y_ = y * (image + k1 * math.pow(r, 2) + k2 * math.pow(r, 4) + k3 * math.pow(r, 6)) + 2 * p2 * x * y + p1 * (r * r + 2 * y * y)
-        # img_point = np.array([x_, y_, image])
-        # x, y, t = np.matmul(self.IntrinsicMatrix, img_point)
-        # return round(x / t), round(y / t)
 
     def caliExtrinsicsMatrix(self, world_points, img_points):
         found, rvec, tvec = cv2.solvePnP(world_points, img_points, self.IntrinsicMatrix, self.Distortion)
@@ -96,7 +78,6 @@ class Camera:
             return
         self.rvec = rvec
         self.tvec = tvec
-        # print(rvec,'\n',tvec)
         self.rotM = cv2.Rodrigues(rvec)[0]
         tmp = np.zeros((4, 4))
         tmp[0:3, 0:3] = self.rotM.copy()
@@ -107,5 +88,3 @@ class Camera:
         tmp1[0:3, 0:3] = self.IntrinsicMatrix.copy()
         self.Matrix = np.matmul(tmp1, self.ExtrinsicsMatrix)
         self._flag = True
-        # print(rotM, tvec)
-        # print(self.ExtrinsicsMatrix)
